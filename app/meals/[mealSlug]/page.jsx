1a76e41eb2935c11fb6958c8cd968eade7d62f93
saied-nasiripour <saied.nasiripour@gmail.com>
2024-05-19 10:15:37 +0330

    Mealdetail Page is done

--- app/meals/[mealSlug]/page.jsx
+++ app/meals/[mealSlug]/page.jsx
@@ -1,7 +1,43 @@
+import Image from 'next/image';
 
-function MealDetailsPage() {
+import classes from './page.module.css';
+import {Fragment} from "react";
+import Link from "next/Link";
+import {getMeal} from "@/lib/meals";
+import {notFound} from "next/navigation";
+
+
+
+function MealDetailsPage({params}) {
+
+    const meal= getMeal(params.mealSlug);
+
+    if (!meal) {
+        notFound();
+    }
+
+    meal.instructions = meal.instructions.replace(/\n/g, '<br />');
+
     return (
-        <h1>Meal Details</h1>
+        <>
+            <header className={classes.header}>
+                <div className={classes.image}>
+                    <Image src={meal.image} alt={meal.title} fill/>
+                </div>
+                <div className={classes.headerText}>
+                    <h1>{meal.title}</h1>
+                    <p className={classes.creator}>
+                        by <Link href={`mailto:${meal.creator_email}`}>{meal.creator}</Link>
+                    </p>
+                    <p className={classes.summary}>{meal.summary}</p>
+                </div>
+            </header>
+            <main>
+                <p className={classes.instructions} dangerouslySetInnerHTML={{
+                    __html: meal.instructions,
+                }}></p>
+            </main>
+        </>
     )
 }
 
--- app/meals/page.jsx
+++ app/meals/page.jsx
@@ -3,13 +3,12 @@ import Link from 'next/link';
 import classes from './page.module.css';
 import MealsGrid from "@/components/meals/meals-grid";
 import {getMeals} from "@/lib/meals";
-
+import MealsLoadingPage from "@/app/meals/loading-out";
 
 async function Meals() {
     const meals = await getMeals();
     return <MealsGrid meals={meals} />;
 }
-
 
 function MealsPage () {
     return (
@@ -27,7 +26,7 @@ function MealsPage () {
                 </p>
             </header>
             <main className={classes.main}>
-                <Suspense fallback={<p className={classes.loading}>Fetching meals...</p>}>
+                <Suspense fallback={<MealsLoadingPage/>}>
                     <Meals />
                 </Suspense>
             </main>
